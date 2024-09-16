f43a9ba11dabdd71c963b1cc79a0573b673a194e
Michel John <michel.santos@dataside.com.br>
2024-09-16 19:43:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <div className="h-screen bg-zinc-950 justify-center items-center flex">
+      <Button>ReactJs Template Base</Button>
+    </div>
+  );
+}
+
+export default App;
